a58383d33354e4930fa7c973ad46803725a29f3b
Mick Knutson <mickknutson@gmail.com>
2017-08-23 07:48:21 -0600

    Small cleanup

--- configserver/src/main/java/com/baselogic/cloud/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/baselogic/cloud/configserver/ConfigserverApplication.java
@@ -1,9 +1,12 @@
 package com.baselogic.cloud.configserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.cors.CorsConfiguration;
@@ -13,42 +16,30 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @EnableConfigServer
+//@Slf4j
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}
 
-    /*@Bean @Order(Ordered.HIGHEST_PRECEDENCE)
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("DELETE");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
-
+    @Profile("trace")
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedHeaders("*")
-                        .allowedMethods("GET", "PUT", "POST")
-                        .allowedOrigins("http://localhost:8888")
-                ;
-            }
-        };
-    }*/
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
 
+            String[] beanNames = ctx.getBeanDefinitionNames();
+
+            System.out.println("\n********************************************");
+            System.out.println("Listing the " + beanNames.length + " beans loaded by Spring Boot:");
+
+            Arrays.stream(beanNames).sorted().forEach(System.out::println);
+
+            System.out.println("*** the End ********************************\n");
+        };
+    }
 }
