8c2563c4ff1ff99c5d71892b4bfe0789151ef1a3
Mick Knutson <mickknutson@gmail.com>
2018-01-18 08:55:27 +0200

    Updated p12 and JKS

--- configserver/src/test/java/com/baselogic/cloud/configserver/ConfigserverApplicationTests.java
+++ configserver/src/test/java/com/baselogic/cloud/configserver/ConfigserverApplicationTests.java
@@ -12,8 +12,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ConfigserverApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
